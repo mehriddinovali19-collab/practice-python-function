4cfa26c33067886660a93e77834f56d6740f0b24
ali mekhridinov <mehriddinovali19@gmail.com>
2026-03-26 13:58:14 +0500

    task01 done

--- task01.py
+++ task01.py
@@ -1,39 +1,40 @@
 def add(a, b):
-    return a + b
+    return a+b
 
 
 def subtract(a, b):
-    return a - b
+    return a -b 
 
 
 def multiply(a, b):
-    return a * b
+    return a*b
 
 
 def divide(a, b):
-    if b != 0:
-        return a / b
+    if b !=0 :
+        return a/b
     else:
         return None
+    
 
 def main():
-    a = float(input('a: '))
-    b = float(input('b: '))
-    op = input('amal: ')
+    a = float(input("a ga son bering: "))
+    b = float(input("b ga son bering: "))
+    op = input("amal: ")
 
-    if op == '+':
+    if op == "+":
         print(add(a, b))
-    elif op == '-':
+    elif op == "-":
         print(subtract(a, b))
-    elif op == '*':
+    elif op == "*":
         print(multiply(a, b))
-    elif op == '/':
+    elif op == "/":
         result = divide(a, b)
         if result == None:
-            print('0 ga bo\'lish mumkin emas.')
+            print("0 ga bo'lish mukin emas!")
         else:
             print(result)
-    else:
-        print('bunday amal mavjud emas.')
+    else: 
+        print("bo'nday amla mavjud emas!")
 
 main()
